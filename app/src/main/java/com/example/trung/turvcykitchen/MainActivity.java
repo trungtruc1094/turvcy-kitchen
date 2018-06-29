8540d9f2250d7392810cd902dd31769ab9673074
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-06-30 03:19:29 +1200

    commit

--- app/src/main/java/com/example/trung/turvcykitchen/MainActivity.java
+++ app/src/main/java/com/example/trung/turvcykitchen/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trung.turvcykitchen;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,28 +11,36 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.NavigationView;
 import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+
 
 // Add NavigationView.OnNavigationItemSelectedListener to make the Activity respond to item being clicked on navigation drawer
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    FragmentTransaction ft;
+    private static final String BACK_STACK_ROOT_TAG = "root_fragment";
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         Intent intent = null;
+        Fragment fragment = null;
         switch (id) {
             case R.id.food_list:
-                intent = new Intent(this, FoodList.class);
+                fragment = new FoodList();
                 break;
             case R.id.menu_list:
-                intent = new Intent(this, MenuList.class);
+                fragment = new MenuList();
                 break;
             default:
                 intent = new Intent(this, EditProfile.class);
         }
 
-        if (intent != null) {
+        if (fragment != null) {
+            addFragment(fragment);
+        } else {
             startActivity(intent);
         }
+
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
@@ -54,6 +63,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Set Food fragment for Main Activity
+        Fragment food_fragment = new FoodList();
+        addFragment(food_fragment);
+
+
         // Get drawer layout
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_main);
         // Create toggle button
@@ -70,5 +84,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
 
+    }
+
+    public void addFragment(Fragment fragment){
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_container, fragment);
+        ft.commit();
+    }
+
+    public void replaceFragment(Fragment fragment){
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_container, fragment);
+        ft.commit();
     }
 }
